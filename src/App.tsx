01b918696a9eb222b5c452e82572744e7c907493
SergeyDedikov <serded59@yandex.ru>
2023-03-28 17:48:23 +0500

    feat: setting HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
+      <Routes>
+        <Route path="/" element={<h2>Home</h2>} />
+        <Route path="/cosmetics" element={<h2>Каталог</h2>} />
+        <Route path="*" element={<h2>404</h2>} />
+      </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
