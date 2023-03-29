fcc04d4e5ede2c2bda8f33c1b31507cea4e1459d
SergeyDedikov <serded59@yandex.ru>
2023-03-29 23:26:41 +0500

    feat: update Product page

--- src/components/ProductsPage.tsx
+++ src/components/ProductsPage.tsx
@@ -1,10 +1,12 @@
 import React, { FC, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { IProduct } from "../types/types";
-import { getProducts } from './api/api';
+import { getProducts } from '../api/api';
+import Container from './Container';
 import List from "./List";
 import ProductItem from "./ProductItem";
+import CategoriesList from './CategoriesList';
 
 
 const ProductsPage: FC = () => {
@@ -22,16 +24,47 @@ const ProductsPage: FC = () => {
   }
 
   return (
-    <List
-      items={products}
-      renderItem={(product: IProduct) =>
-        <ProductItem
-          onClick={(product) => navigate('/catalog/' + product.id)}
-          product={product}
-          key={product.id}
-        />}
+    <Container>
+      <nav>
+        <ul className='breadcrumbs'>
+          <li>
+            <Link to={'/'}>Главная</Link>
+          </li>
+          <li>Косметика и гигиена</li>
+        </ul>
+      </nav>
 
-    />
+      <h2 className='page-title'>
+        Косметика и гигиена
+      </h2>
+
+      <div id='selectSort'>
+        <span>Сортировка: </span>
+        <select>
+          <optgroup label='Название'>
+            <option defaultValue={'nameAscending'}>Название по возрастанию</option>
+            <option value='nameDescending'>Название по убыванию</option>
+          </optgroup>
+          <optgroup label='Цена'>
+            <option value="priceAscending">Цена по возрастанию</option>
+            <option value="priceDescending">Цена по убыванию</option>
+          </optgroup>
+        </select>
+      </div>
+
+      <CategoriesList />
+
+      <List
+        className='product-list'
+        items={products}
+        renderItem={(product: IProduct) =>
+          <ProductItem
+            onClick={(product) => navigate('/catalog/' + product.id)}
+            product={product}
+            key={product.id}
+          />}
+      />
+    </Container>
   );
 };
 
