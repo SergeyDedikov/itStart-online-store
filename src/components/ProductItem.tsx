69e2cf8e196f3a2cc2331de4989b221559aaeb62
SergeyDedikov <serded59@yandex.ru>
2023-03-29 23:24:29 +0500

    feat: add all parameters to card of products

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react';
+
 import { IProduct } from "../types/types";
-import { DB_URL } from './api/api';
+import { DB_URL } from '../api/api';
+import { formatAmount } from '../utils/utils';
+import Button from './UI/Button';
 
 interface ProductItemProps {
   product: IProduct;
@@ -8,15 +11,66 @@ interface ProductItemProps {
 }
 
 const ProductItem: FC<ProductItemProps> = ({ product, onClick }) => {
+
+  const volumeString =
+    product.quantity_in_package && product.quantity_in_package > 1
+      ? `${product.quantity_in_package}X${product.volume} ${product.measure_unit}`
+      : `${product.volume} ${product.measure_unit}`;
+
+  let volumeIconClassName: string = '';
+
+  switch (product.measure_unit) {
+    case 'г':
+      volumeIconClassName = 'card__volume-icon--box';
+      break;
+
+    case 'мл':
+      volumeIconClassName = 'card__volume-icon--bottle';
+      break;
+
+  }
+
   return (
-    <div
-      onClick={() => onClick(product)}
-      style={{ padding: 15, border: '1px solid gray' }}>
-      <img src={DB_URL + product.image.small?.url} alt={product.name}/>
-      <h2>
-        {product.id}. {product.name}
-      </h2>
-      {product.description}
+    <div className='card'>
+      <img
+        className='card__image'
+        src={DB_URL + product.image.small?.url}
+        alt={product.name} />
+
+      <div className='card__volume'>
+        <span className={`card__volume-icon ${volumeIconClassName}`}></span>{volumeString}
+      </div>
+      <h3
+        className='card__title'
+        onClick={() => onClick(product)}
+      >
+        <span>{product.brand.toLocaleUpperCase()}</span> {product.name}
+      </h3>
+
+      <ul className="card__parameters">
+        <li>
+          Штрихкод: <span>{product.barcode}</span>
+        </li>
+        <li>
+          Производитель: <span>{product.manufacturer}</span>
+        </li>
+        <li>
+          Бренд: <span>{product.brand.toUpperCase()}</span>
+        </li>
+      </ul>
+
+      <div className="card__action">
+        <span className='card__price'>
+          {formatAmount(product.price)} &#8376;
+        </span>
+        <Button
+          className='card__button'
+          status='buy'
+        >
+          В корзину
+        </Button>
+      </div>
+
     </div>
   );
 };
