c2a3c081dca5070f9ec3ab9e1b9517c2acd2ab26
SergeyDedikov <serded59@yandex.ru>
2023-03-29 23:25:26 +0500

    feat: update Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { useNavigate, useNavigation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import './Header.sass';
 import Container from './Container';
