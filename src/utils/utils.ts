70f1e3beadfcba68645cdedaa24c04608e6ec4f0
SergeyDedikov <serded59@yandex.ru>
2023-03-30 21:47:48 +0500

    feat: add utils compare

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,3 +4,20 @@ export function formatAmount(amount: number): string {
     ? new Intl.NumberFormat('ru-RU', { minimumFractionDigits: 2 }).format(amount)
     : '';
 }
+
+/**
+ * Generic typed comparison fn for use with `Array.sort`
+ * This is passed as the comparison function with
+ * the following syntax:
+ * MyArray.sort(compareObjectsByKey<MyObjectType>('myObjectKey', true/false)`
+ * @type T
+ * @param key: keyof T
+ * @param ascending: boolean
+ */
+export function compareObjectsByKey<T>(key: keyof T, ascending = true) {
+  return function innerSort(objectA: T, objectB: T) {
+    const sortValue =
+      objectA[key] > objectB[key] ? 1 : objectA[key] < objectB[key] ? -1 : 0;
+    return ascending ? sortValue : -1 * sortValue;
+  };
+}
